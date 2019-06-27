c97e5fb291c127a792f9b81016b768470b0b387e
Mads H. Danquah <mads@danquah.dk>
2019-06-27 16:52:59 +0200

    New ingress controller config

--- installation/universal/01-setup-ingress-controller.sh
+++ installation/universal/01-setup-ingress-controller.sh
@@ -6,7 +6,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${SCRIPT_DIR}"
 source "_settings.sh"
 
-helm install \
-    --name nginx-ingress stable/nginx-ingress \
+helm upgrade \
+    --install \
+    --namespace default \
     --set rbac.create=true \
-    --set controller.service.loadBalancerIP="${EXTERNAL_IP}"
+    --set controller.kind=DaemonSet \
+    --set controller.publishService.enabled=true \
+    --set controller.service.loadBalancerIP="${EXTERNAL_IP}" \
+    nginx-ingress \
+    stable/nginx-ingress
+
+kubectl apply -n default -f ./nginx-ingress/configmap.yaml
