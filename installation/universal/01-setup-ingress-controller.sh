1d75aef934d10c040a00aa7751e2b99a5bb5b86d
Mads H. Danquah <mads@danquah.dk>
2020-11-13 15:50:11 +0100

    fixed helm upgrade syntax and nginx-ingress -> ingress-nginx

--- installation/universal/01-setup-ingress-controller.sh
+++ installation/universal/01-setup-ingress-controller.sh
@@ -8,14 +8,12 @@ source "_settings.sh"
 
 kubectl create --dry-run=client -o yaml namespace ingress-nginx | kubectl apply -f
 
-kubectl apply -n ingress-nginx -f ./nginx-ingress/configmap.yaml
+kubectl apply -n ingress-nginx -f ./ingress-nginx/configmap.yaml
 
 helm upgrade \
     --install \
     --namespace ingress-nginx \
     --set rbac.create=true \
     --set controller.kind=DaemonSet \
-    --set controller.publishService.enabled=true \
     --set controller.service.loadBalancerIP="${EXTERNAL_IP}" \
-    --name ingress-nginx \
-    ingress-nginx/ingress-nginx
+    ingress-nginx ingress-nginx/ingress-nginx
